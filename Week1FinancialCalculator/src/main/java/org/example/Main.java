743e6474922ee1b86eed64f258029b0e4a7bbf37
HuntR-exe <jg203091@gmail.com>
2025-04-11 10:01:06 -0500

    Update1

--- Week1FinancialCalculator/src/main/java/org/example/Main.java
+++ Week1FinancialCalculator/src/main/java/org/example/Main.java
@@ -1,7 +1,29 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Welcome to the Mortgage Calculator! %n");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the loan amount: ");
+        double principal = scanner.nextDouble();
+
+        System.out.println("Enter the annual interest rate as a decimal: ");
+        double annualInterestRate = scanner.nextDouble();
+
+        System.out.println("Enter length of loan in years: ");
+        int numberOfMonths = scanner.nextInt();
+
+
+
+
+
+
+
+
 
     }
 }
